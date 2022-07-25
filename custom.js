aa19374ed01d7a0b715ff912f66e534a0d78bcac
Sibgatullah001 <msmmunna8@gmail.com>
2022-07-26 00:57:13 +0600

    added20

--- custom.js
+++ custom.js
@@ -1,8 +1,53 @@
-$('#menu , #toggle_icon').click(function(){
-    $('#menu').toggleClass('show_menu');
+const menuPopup = document.getElementById('menu');
+
+function togglePopup(){
+    menuPopup.style.display = 'block';
+
+};
+function crossPopup(){
+    menuPopup.style.display = 'none';
+};
+
+
+// display none / block
+const apps = document.getElementById('apps');
+const recommended = document.getElementById('recommended');
+const appHeader = document.getElementById('app_data');
+
+
+function onlyApp(){
+    recommended.style.display = "none";
+    appHeader.style.display = "none";
+    apps.style.display = "block";
+}
+
+
+function allSection(){
+    recommended.style.display = "block";
+    appHeader.style.display = "block";
+    apps.style.display = "none";
+}
+
+
+function onlyRecommended(){
+    recommended.style.display = "block";
+    apps.style.display = "none";
+    appHeader.style.display = "none";
+}
+
+// search input 
+const searchInput = document.getElementById('search');
+
+searchInput.addEventListener('input', function (e){
+    const value = e.target.value;
+    console.log(value);
 })
 
-
-
-
-
+// search  - - input
+const searchPopup = document.getElementById('searchPopup');
+function searchImg(){
+    searchPopup.style.display = 'block';
+}
+function searchImgClose(){
+    searchPopup.style.display = 'none';
+}
